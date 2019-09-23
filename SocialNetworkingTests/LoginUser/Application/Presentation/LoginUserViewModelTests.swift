62ba7a73ecb1423f6293838c6c515788d7bac8c0
Pablo Manuelli <pablo.manuelli@etermax.com>
2019-09-22 22:55:20 -0300

    Testing login button enabled logic

--- SocialNetworkingTests/LoginUser/Application/Presentation/LoginUserViewModelTests.swift
+++ SocialNetworkingTests/LoginUser/Application/Presentation/LoginUserViewModelTests.swift
@@ -9,11 +9,38 @@ class LoginUserViewModelTests: XCTestCase {
 
     // RxTest elements
     private let scheduler = TestScheduler(initialClock: 0)
+    private var loginUserButtonEnabledObserver: TestableObserver<Bool>!
     private var registerUserButtonTouchObserver: TestableObserver<Void>!
     private let disposeBag = DisposeBag()
     
     // Object under test
     private var viewModel: LoginUserViewModel!
+    
+    func testInitiallyDisablesRegisterUserButton() {
+         
+         whenViewModelIsCreated()
+         
+         thenLoginUserButtonIsDisabled()
+     }
+     
+     func testEnablesRegisterUserButtonWhenFieldsAreNotEmpty() {
+         
+         givenAViewModel()
+         
+         whenFieldsAreSetTo(username: "username", password: "password")
+         
+         thenLoginUserButtonIsEnabled()
+     }
+     
+     func testDisablesRegisterUserButtonWhenAFieldIsEmpty() {
+         
+         givenAViewModel()
+         givenAViewModelWithCompletedFields()
+         
+         whenUsernameChangesTo("")
+         
+         thenLoginUserButtonIsDisabled()
+     }
     
     func testInformsWhenRegisterUserButtonIsTouched() {
         
@@ -30,7 +57,17 @@ class LoginUserViewModelTests: XCTestCase {
         
         viewModel = LoginUserViewModel()
         
+        subscribeToLoginButtonEnabled()
         subscribeToRegisterUserButtonTouch()
+    }
+    
+    private func subscribeToLoginButtonEnabled() {
+        
+        loginUserButtonEnabledObserver = scheduler.createObserver(Bool.self)
+        
+        viewModel.output.loginUserButtonEnabled
+            .drive(loginUserButtonEnabledObserver)
+            .disposed(by: disposeBag)
     }
     
     private func subscribeToRegisterUserButtonTouch() {
@@ -42,13 +79,49 @@ class LoginUserViewModelTests: XCTestCase {
             .disposed(by: disposeBag)
     }
     
+    private func givenFieldsSetTo(username: String, password: String) {
+        
+        viewModel.input.username.accept(username)
+        viewModel.input.password.accept(password)
+    }
+    
+    private func givenAViewModelWithCompletedFields() {
+        
+        givenAViewModel()
+        givenFieldsSetTo(username: "u", password: "p")
+    }
+
     // MARK: When
+    
+    private func whenViewModelIsCreated() {
+        givenAViewModel()
+    }
+    
+    private func whenFieldsAreSetTo(username: String, password: String) {
+        givenFieldsSetTo(username: username, password: password)
+    }
+    
+    private func whenUsernameChangesTo(_ text: String) {
+        viewModel.input.username.accept(text)
+    }
     
     private func whenRegisterUserButtonIsTouched() {
         viewModel.registerUserButtonTouched()
     }
     
     // MARK: Then
+    
+    private func thenLoginUserButtonIsEnabled() {
+        
+        let lastValue = loginUserButtonEnabledObserver.events.last?.value.element
+        XCTAssertEqual(lastValue, true)
+    }
+    
+    private func thenLoginUserButtonIsDisabled() {
+        
+        let lastValue = loginUserButtonEnabledObserver.events.last?.value.element
+        XCTAssertEqual(lastValue, false)
+    }
     
     private func thenRegisterUserButtonTouchIsEmmited() {
         XCTAssertEqual(registerUserButtonTouchObserver.events.count, 1)
