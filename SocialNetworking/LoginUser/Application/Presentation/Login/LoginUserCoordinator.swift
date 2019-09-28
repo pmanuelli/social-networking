4a250f2195ffeae9c691a0158a82fbb3955b42ad
Pablo Manuelli <pablo.manuelli@etermax.com>
2019-09-27 21:06:49 -0300

    Changing project structure

---
+++ SocialNetworking/LoginUser/Application/Presentation/Login/LoginUserCoordinator.swift
@@ -0,0 +1,67 @@
+
+import UIKit
+import RxSwift
+
+class LoginUserCoordinator {
+    
+    private let navigationController: UINavigationController
+    private let userRepository: UserRepository
+    
+    private lazy var userService = UserServiceDefault(userRepository: userRepository)
+    private lazy var loginUser = LoginUserDefault(userService: userService)
+    private lazy var registerUser = RegisterUserDefault(userService: userService)
+    
+    private let disposeBag = DisposeBag()
+    
+    init(navigationController: UINavigationController, userRepository: UserRepository) {
+        self.navigationController = navigationController
+        self.userRepository = userRepository
+    }
+    
+    func start() {
+        
+        goToLoginUser()
+    }
+    
+    private func goToLoginUser() {
+        
+        let viewModel = LoginUserViewModel(loginUser: loginUser)
+        let viewController = LoginUserViewController(viewModel: viewModel)
+        
+        subscribe(to: viewModel)
+        
+        pushViewController(viewController)
+    }
+    
+    private func subscribe(to viewModel: LoginUserViewModel) {
+        
+        viewModel.output.registerUserButtonTouch
+            .subscribe(onNext: { [weak self] _ in self?.goToRegisterUser() })
+            .disposed(by: disposeBag)
+    }
+    
+    private func goToRegisterUser() {
+        
+        let viewModel = RegisterUserViewModel(registerUser: registerUser)
+        let viewController = RegisterUserViewController(viewModel: viewModel)
+        
+        subscribe(to: viewModel)
+        
+        pushViewController(viewController)
+    }
+    
+    private func subscribe(to viewModel: RegisterUserViewModel) {
+        
+        viewModel.output.loginUserButtonTouch
+            .subscribe(onNext: { [weak self] _ in self?.popViewController() })
+            .disposed(by: disposeBag)
+    }
+    
+    private func pushViewController(_ viewController: UIViewController) {
+        navigationController.pushViewController(viewController, animated: true)
+    }
+    
+    private func popViewController() {
+        navigationController.popViewController(animated: true)
+    }
+}
